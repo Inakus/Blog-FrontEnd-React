a1200274596c6dcd9f85aa14843b05a51dd3c994
Karol Dobrek <Inakus@wp.pl>
2023-04-11 15:46:06 +0200

    change variable

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import AllBlogs from "./Component/BlogPost/AllBlogs";
-import Navbar from "./Component/Navbar"
+import Navbar from "./Component/Navbar";
 import Search from "./Component/Search/Search";
 import Home from "./Component/Home";
 import Post from "./Component/Postform/Post";
@@ -12,46 +12,48 @@ import BlogPost from "./Component/BlogPost/BlogPost";
 import Error404 from "./Component/Error404";
 import Edit from "./Component/Postform/Edit";
 
-export const apiUrl = 'http://localhost:4000/'
+export const apiUrl = process.env.REACT_APP_APIURL;
 
 const client = axios.create({
-  baseURL: apiUrl
+  baseURL: apiUrl,
 });
 
-
 function App() {
-  const [post, setPost] = useState(null)
+  const [post, setPost] = useState(null);
 
   useEffect(() => {
-    async function getPost(){
-      const response = await client.get('/').catch(err => console.log(err));
-      setPost(response.data)
+    async function getPost() {
+      const response = await client.get("/").catch((err) => console.log(err));
+      setPost(response.data);
     }
-    getPost()
-  }, [post])
+    getPost();
+  }, [post]);
 
-  function deletePost(newId){
-    return axios.delete(apiUrl + newId).then(alert('Post Deleted!'))
+  function deletePost(newId) {
+    return axios.delete(apiUrl + newId).then(alert("Post Deleted!"));
   }
 
-  if(!post) return null;
+  if (!post) return null;
 
   return (
-   <>
-   <BrowserRouter>
-      <Navbar></Navbar>
-    <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/post" element={<Post url={apiUrl}/>}></Route>
-        <Route path="/search" element={<Search post={post} deletePost={deletePost}/>}></Route>
-        <Route path="/blogs" element={<AllBlogs post={post}/>}></Route>
-        <Route path="/blog/:id" element={<BlogPost post={post}/>}></Route>
-        <Route path="/blog/:id/edit" element={<Edit />}></Route>
-        <Route path="*" element={<Error404 />}></Route>
-    </Routes>
-   </BrowserRouter>
-   </>
-  )
+    <>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/post" element={<Post url={apiUrl} />}></Route>
+          <Route
+            path="/search"
+            element={<Search post={post} deletePost={deletePost} />}
+          ></Route>
+          <Route path="/blogs" element={<AllBlogs post={post} />}></Route>
+          <Route path="/blog/:id" element={<BlogPost post={post} />}></Route>
+          <Route path="/blog/:id/edit" element={<Edit />}></Route>
+          <Route path="*" element={<Error404 />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
